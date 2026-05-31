94a78111b07ade4355a729396a114484718fbbd7
unknown <Arishka.drachenko@mail.ru>
2026-05-31 21:04:37 +0300

    model adding

---
+++ Model/Core/Interface/IChangeable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Core
+{
+    public interface IChangeable
+    {
+        bool AddPet(Pet pet);// Метод добавления питомца
+        void RemovePet(Pet pet); // Метод удаления питомца
+    }
+}
---
+++ Model/Core/Interface/ICountable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Core
+{
+    public interface ICountable
+    {
+        int Count(); // число животных
+        int Count(Type type); // число животных конкретного типа
+        int Percentage(Type type); // % от общего числа 
+    }
+}
